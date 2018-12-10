143c5c1d246c8f3220b73d112a685d501b271d24
vedo <vedadr@gmail.com>
2018-12-10 20:47:20 +0100

    minor fixes

--- raffaparse.py
+++ raffaparse.py
@@ -1,10 +1,11 @@
 import httplib2
 import os
-import base64 #used to decode attachment
-from bs4 import BeautifulSoup #for attachment parsing
-import pandas as pd #for storage prior to sqlite3
-from dateutil.parser import parse as dtpa #used to check if first cell contained date info
+import base64  # used to decode attachment
+from bs4 import BeautifulSoup  # for attachment parsing
+import pandas as pd  # for storage prior to sqlite3
+from dateutil.parser import parse as dtpa  # used to check if first cell contained date info
 import sqlite3
+import pickle
 
 from apiclient import discovery, errors
 from oauth2client import client
@@ -25,8 +26,7 @@ def get_credentials():
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
+    credential_path = os.path.join(credential_dir, 'gmail-python-quickstart.json')
 
     store = Storage(credential_path)
     credentials = store.get()
@@ -35,29 +35,29 @@ def get_credentials():
         flow.user_agent = APPLICATION_NAME
         if flags:
             credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
+        # else:  # Needed only for compatibility with Python 2.6
+        #     credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
 
 
-def getRaffaMails(service, user_id, query):
+def get_raffa_mails(service, user_id, query):
     try:
         response = service.users().messages().list(userId=user_id, q=query).execute()
         messages = []
         if 'messages' in response:
-          messages.extend(response['messages'])
+            messages.extend(response['messages'])
         while 'nextPageToken' in response:
-          page_token = response['nextPageToken']
-          response = service.users().messages().list(userId=user_id, q=query,
-                                                     pageToken=page_token).execute()
-          messages.extend(response['messages'])
-        return (messages)
+            page_token = response['nextPageToken']
+            response = service.users().messages().list(userId=user_id, q=query,
+                                                       pageToken=page_token).execute()
+            messages.extend(response['messages'])
+        return messages
     except errors.HttpError as error:
         print('An error occurred: {}'.format(error))
 
 
-def isDateTime(testString):
+def is_date_time(testString):
     try:
         dtpa(testString)
         return True
@@ -68,58 +68,93 @@ def isDateTime(testString):
 def scrapeStatementForInfo(attachment):
     data = []
     soup = BeautifulSoup(attachment, "lxml")
-    rows = soup.find_all('tr')
-    startInTable = False
-    rowList = []
-    for td in rows:
-        rowData = td.find_all('td')
-        if (rowData):
-            for rowElem in rowData:
-                rowText = rowElem.find(text = True)
-                if (rowText):
-                    if (isDateTime(rowText.replace('Datum: ', ''))):
-                        if (rowList):
-                            data.append(rowList)
-                        startInTable = True
-                        rowList = []
-                    if (startInTable):
-                        rowList.append(rowText)
-    return data
+    tables = soup.find_all('table')[7:]
+    data_content = []
+    for i, table in enumerate(tables):
+        cells = table.find_all('td')
+        cells_content = [c.text for c in cells]
 
-def downloadMails(raffaMailIds, service, user_id):
-    messagesHolder = []
-    attachHolder = []
-    tempCount = 0
-    if tempCount < 2:
-        for el in raffaMailIds:
-            downloadedMsg = service.users().messages().get(userId=user_id, id=el['id']).execute()
-            for part in downloadedMsg['payload']['parts']:
-                if part['filename'] and part['mimeType'] == 'text/html':
-                    attachmentId = part['body']['attachmentId']
-                    attachmentData = service.users().messages().attachments().get(userId = user_id, messageId = el, id = attachmentId).execute()
-                    attachHolder.append(scrapeStatementForInfo(base64.urlsafe_b64decode(attachmentData['data'].encode('UTF-8'))))
-            tempCount = tempCount + 1
-        return (messagesHolder)
+        if i == 0:
+            columns = cells_content
+        else:
+            if is_date_time(cells_content[0]):
+                data_content.append(cells_content)
+            else:
+                break
+    email_content = pd.DataFrame(data=data_content, columns=columns)
+
+    return email_content
+
+
+def fetch_mails(raffa_mail_ids, service, user_id):
+    """
+    This will download emails with selected ids
+    :param raffa_mail_ids: list of mail id's
+    :param service:
+    :param user_id:
+    :return: list of messages
+    """
+    temp_count = 0
+    if temp_count < 2:
+        if pickle_mail_content:
+            attach_holder_raw = []
+            for el in raffa_mail_ids:
+                downloaded_msg = service.users().messages().get(userId=user_id, id=el['id']).execute()
+                for part in downloaded_msg['payload']['parts']:
+                    if part['filename'] and part['mimeType'] == 'text/html':
+                        attachment_id = part['body']['attachmentId']
+                        attachment_data = service.users().messages().attachments().get(userId=user_id, messageId=el,
+                                                                                       id=attachment_id).execute()
+                        attach_holder_raw.append(base64.urlsafe_b64decode(attachment_data['data'].encode('UTF-8')))
+                temp_count = temp_count + 1
+            pickle.dump(attach_holder_raw, open('attachments_raw.pkl', 'wb'))
+        else:
+            attach_holder_raw = pickle.load(open('attachments_raw.pkl', 'rb'))
+
+    return attach_holder_raw
+
+
+def process_mails(mail_att_extra):
+    attachment_holder = []
+    for attachment_raw in mail_att_extra:
+        attachment_holder.append(scrapeStatementForInfo(attachment_raw))
+    processed_emails = pd.concat(attachment_holder)
+    processed_emails.to_sql('usage_data', con=conn)
+
 
 def main():
+    # prepare authentication
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
-    raffaMailIds = getRaffaMails(service, 'me', 'from:E-izvodi.Raiffeisenbanka@rbbh.ba')
-    cur = conn.cursor()
-    cur.execute('DROP TABLE IF EXISTS messageIds')
-    cur.execute('''CREATE TABLE IF NOT EXISTS messageIds(
-                    threadId TEXT,
-                    msgID TEXT
-                    )''')
-    cur.executemany('INSERT INTO messageIds (threadId, msgId) VALUES (:threadId, :id);', raffaMailIds)
-    messageList = downloadMails(raffaMailIds, service, 'me')
+    if update_mails:
+        # get mail ids
+        raffa_mail_ids = get_raffa_mails(service, 'me', 'from:E-izvodi.Raiffeisenbanka@rbbh.ba')
 
-    print('stop here')
+        # prepare db and store mail id's
+        cur = conn.cursor()
+        cur.execute('DROP TABLE IF EXISTS messageIds')
+        cur.execute('''CREATE TABLE IF NOT EXISTS messageIds(
+                        threadId TEXT,
+                        msgID TEXT
+                        )''')
+        cur.executemany('INSERT INTO messageIds (threadId, msgId) VALUES (:threadId, :id);', raffa_mail_ids)
+    else:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM messageIds')
+        raffa_mail_ids = cur.fetchall()
+    # download mails
+    mail_attachments_extracted = fetch_mails(raffa_mail_ids, service, 'me')
+    process_mails(mail_attachments_extracted)
+
 
 if __name__ == '__main__':
-    reloadAllMessages = False
+    # reloadAllMessages = False
+    # if set to true script will check if there are new mails available
+    update_mails = False
+    # if set to false script will use pickled email attachments (won't download new ones)
+    pickle_mail_content = False
 
     try:
         import argparse
